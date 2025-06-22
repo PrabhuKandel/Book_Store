0f5964ca96dcf5797bcbbe814a9035b3efe9f724
Prabhu Kandel <kandelprabhu111@gmail.com>
2025-06-22 22:15:20 +0545

    modularize project structure, implement repository + unit of work pattern, category CRUD

---
+++ BookStore.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.DataAccess.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        //T - Category
+        IEnumerable<T> GetAll();
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+
+        // we will not include update here because its implementation will differ between entity
+        //void Update(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+
+
+    }
+}
